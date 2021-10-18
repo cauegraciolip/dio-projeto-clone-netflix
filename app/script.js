4d3b7ef3c9116fd3baf90eb3ae8f279709293345
cauegraciolip <cauegracioli@outlook.com>
2021-10-17 23:51:58 -0300

    finish description modal, correction header responsivity, add js button show and hidde description

--- app/script.js
+++ app/script.js
@@ -1,3 +1,4 @@
+/* ===== NAV - PROFILE - SEARCH ===== */
 const search = document.getElementById('lupa')
 const nav = document.getElementById('nav')
 const header = document.getElementById('header')
@@ -12,3 +13,22 @@ function showMenu() {
 }
 
 search.addEventListener('click', showInput)
+
+/* ===== SHOW AND HIDDE DESCRIPTION ===== */
+
+const exitButton = document.getElementById('exit-description')
+const moreInformationButton = document.getElementById('more-information')
+
+const infoSeries = document.getElementById('info-serie')
+
+function hiddeDescription() {
+    infoSeries.classList.toggle('description-display')
+}
+
+function showDescription() {
+    infoSeries.classList.toggle('description-display')
+}
+
+
+exitButton.addEventListener('click', hiddeDescription)
+moreInformationButton.addEventListener('click', showDescription)
