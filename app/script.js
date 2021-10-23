adcfd94e5cc391eaa682b3d5e338c49a6b5246d1
cauegraciolip <cauegracioli@outlook.com>
2021-10-22 21:17:01 -0300

    add footer, refactoring, edit carousel button

--- app/script.js
+++ app/script.js
@@ -1,55 +1,54 @@
 /* ===== NAV - PROFILE - SEARCH ===== */
-const search = document.getElementById('lupa')
-const nav = document.getElementById('nav')
-const header = document.getElementById('header')
+const search = document.getElementById("lupa");
+const nav = document.getElementById("nav");
+const header = document.getElementById("header");
 
 function showInput() {
-    const pesquisa = document.getElementById('pesquisa')
-    pesquisa.classList.toggle('pesquisaActive')
+  const pesquisa = document.getElementById("pesquisa");
+  pesquisa.classList.toggle("pesquisaActive");
 }
 
 function showMenu() {
-    nav.classList.toggle('activeMenu') 
+  nav.classList.toggle("activeMenu");
 }
 
-search.addEventListener('click', showInput)
+search.addEventListener("click", showInput);
 
 /* ===== SHOW AND HIDDE DESCRIPTION ===== */
 
-const exitButton = document.getElementById('exit-description')
-const moreInformationButton = document.getElementById('more-information')
+const exitButton = document.getElementById("exit-description");
+const moreInformationButton = document.getElementById("more-information");
 
-const infoSeries = document.getElementById('info-serie')
+const infoSeries = document.getElementById("info-serie");
 
 function hiddeDescription() {
-    infoSeries.classList.remove('description-display')
+  infoSeries.classList.remove("description-display");
 }
 
 function showDescription() {
-    infoSeries.classList.add('description-display')
+  infoSeries.classList.add("description-display");
 }
 
-
-exitButton.addEventListener('click', hiddeDescription)
-moreInformationButton.addEventListener('click', showDescription)
+exitButton.addEventListener("click", hiddeDescription);
+moreInformationButton.addEventListener("click", showDescription);
 
 /* ===== ROLL CAROUSEL ===== */
 
-$('.owl-carousel').owlCarousel({
-    stagePadding: 20,
-    slideBy: 3,
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+$(".owl-carousel").owlCarousel({
+  stagePadding: 20,
+  slideBy: 3,
+  loop: true,
+  margin: 10,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
